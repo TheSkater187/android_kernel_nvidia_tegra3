f788b411f5e7dc95f7e7848c1d134e2526c9549b
csabaszalonna <csabaszalonna@users.noreply.github.com>
2017-06-23 11:54:20 +0200

    staging: android: lowmemorykiller: neglect swap cached pages in other _file
    With ZRAM enabled it is observed that lowmemory killer
    doesn't trigger properly. swap cached pages are
    accounted in NR_FILE, and lowmemorykiller considers
    this as reclaimable and adds to other_file. But these
    pages can't be reclaimed unless lowmemorykiller triggers.
    So subtract swap pages from other_file.
    
    Signed-off-by: Vinayak Menon <vinayakm.list@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    
    (cherry picked from commit 058dbde)
    
    Change-Id: I217e831bbe1db830e6d61c7943e442a32a7548a1
    
    +
    
    lowmemorykiller: account for unevictable pages
    lowmemorykiller was not taking into account unevictable pages when
    deciding what level to kill. If significant amounts of memory were
    pinned, this caused lowmemorykiller to effectively stop at a much higher
    level than it should.
    
    bug 31255977
    
    Change-Id: I763ecbfef8c56d65bb8f6147ae810692bd81b6e2
    
    +
    
    Include fix

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -38,6 +38,9 @@
 #include <linux/oom.h>
 #include <linux/sched.h>
 #include <linux/swap.h>
+#ifdef CONFIG_NVMAP_ALLOW_SYSMEM
+#include <linux/fs.h>
+#endif
 #include <linux/rcupdate.h>
 #include <linux/notifier.h>
 
@@ -79,7 +82,9 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	int array_size = ARRAY_SIZE(lowmem_adj);
 	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
 	int other_file = global_page_state(NR_FILE_PAGES) -
-						global_page_state(NR_SHMEM);
+						global_page_state(NR_SHMEM) -
+						global_page_state(NR_UNEVICTABLE) -
+ 						total_swapcache_pages;
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
